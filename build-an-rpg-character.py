8a7ae32fd7848e56f67a2ce4f9fb4a983469de43
Daniel <danielnkurunziza@gmail.com>
2026-02-24 15:47:44 +0000

    feat: implement character creation function with validation
    Added validation for character creation parameters including name and stats.

---
+++ build-an-rpg-character.py
@@ -0,0 +1,28 @@
+full_dot = '●'
+empty_dot = '○'
+
+# print(f'{full_dot * 3}{empty_dot*7}')
+
+def create_character(character_name, strength, intelligence, charisma):
+    if not isinstance(character_name, str):
+        return 'The character name should be a string'
+    elif character_name == '':
+        return 'The character should have a name'
+    elif len(character_name) > 10:
+        return 'The character name is too long'
+    elif ' ' in character_name:
+        return 'The character name should not contain spaces'
+
+    if not (isinstance(strength, int) and isinstance(intelligence, int) and isinstance(charisma, int)):
+        return 'All stats should be integers'
+    elif strength < 1 or intelligence < 1 or charisma < 1:
+        return 'All stats should be no less than 1'
+    elif strength > 4 or intelligence > 4 or charisma > 4:
+        return 'All stats should be no more than 4'
+    elif (strength + intelligence + charisma) != 7:
+        return 'The character should start with 7 points'
+
+    return f'{character_name}\nSTR {full_dot * strength}{empty_dot * (10 - strength)}\nINT {full_dot * intelligence}{empty_dot * (10 - intelligence)}\nCHA {full_dot * charisma}{empty_dot * (10 - charisma)}'
+
+character = create_character('dan', 4, 2, 1)
+print(character)
